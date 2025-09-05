2bdcb6386e399a4f663cc3823ab0ce6a007d5ea0
BlueShirtStudios <daniepotgieter910@gmail.com>
2025-09-05 14:37:27 +0200

    Upgraded Crawler Features

--- knowledge_base/knowledge_config.py
+++ knowledge_base/knowledge_config.py
@@ -1,18 +1,18 @@
 CONFIGS = [
     { 
         "api": "https://subnautica.fandom.com/api.php",
-        "output_path": "subnautica_wiki.jsonl", #"subnautica_wiki.jsonl"
+        "output_path": "subnautica_wiki.jsonl",
         "params": {
                 "action": "query",
                 "format": "json",
-                "prop": "info|extracts|pageimages|categories",
+                "prop": "info|extracts|pageimages|categories|sections|images",
                 "inprop": "url",
                 "generator": "allpages",
-                "explaintext": True,   # no HTML, plain text
+                "explaintext": True,   #no HTML, plain text
                 "piprop": "thumbnail",
-                "clshow": "!hidden",   # only visible categories
+                "clshow": "!hidden",   #only visible categories
                 "cllimit": "max",
-                "gaplimit": "10"       # number of pages per batch
+                "gaplimit": "10"       #number of pages per batch
             }
     }
     
